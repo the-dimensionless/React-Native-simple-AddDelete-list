c5089c496909db057bfa58c0a44a393ed9d1d1e1
the-dimensionless <sumit.sssingh.singh45@gmail.com>
2020-08-25 05:08:42 +0530

    Styling buttons and cancel operation

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput, View, Button, FlatList } from 'react-native';
+import { StyleSheet, View, Button, FlatList } from 'react-native';
 
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
@@ -7,39 +7,37 @@ import GoalInput from './components/GoalInput';
 export default function App() {
 
   const [goals, setGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
 
   const addGoalHandler = (goalTitle, callbackClearInput) => {
     console.log('User entered goal as ', goalTitle);
-    console.log('\nUpdating . . .\n');
-
     setGoals(currentGoals => [...currentGoals,
     {
       key: Math.random().toString(),
       value: goalTitle
     }]);
-
-    // Clearing user input
-    //setUserInput('');
+    // Clearing user input or setUserInput('');
     callbackClearInput('');
+    setIsAddMode(false);
   };
 
   /*
-
+     JS filter return true to keep value in array, return false to delete
   */
   const removeGoalHandler = goalId => {
     setGoals(currentGoals => {
-      // JS filter return true to keep value in array
-      // return false to delete
-      console.log('received id is ', goalId);
-      const newValue = currentGoals.filter((goal) => goal.key !== goalId);
-      console.log(newValue);
-      return newValue;
+      return currentGoals.filter((goal) => goal.key !== goalId);;
     });
+  }
+
+  const cancelInputModal = () => {
+    setIsAddMode(false);
   }
 
   return (
     <View style={styles.screen}>
-      <GoalInput onAddGoal={addGoalHandler} />
+      <Button title='Add new Goal' onPress={() => setIsAddMode(true)} />
+      <GoalInput visible={isAddMode} onAddGoal={addGoalHandler} cancel={cancelInputModal} />
       <FlatList
         keyExtractor={(item, index) => item.key}
         data={goals}
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,38 +1,58 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+import { View, TextInput, Button, StyleSheet, Modal } from 'react-native';
 
 const GoalInput = (props) => {
     const [userInput, setUserInput] = useState('');
 
     const userInputHandler = (input) => {
-        console.log('User entered ', input);
+        // console.log('User entered ', input);
         setUserInput({ input });
     };
 
     return (
-        <View style={styles.inputContainer}>
-            <TextInput placeholder='Course Goal'
-                style={styles.input} onChangeText={userInputHandler}
-                value={userInput.input}
-            />
-            <Button title='Add' onPress={props.onAddGoal.bind(this, userInput.input, setUserInput)} />
-        </View>
+        <Modal visible={props.visible} animationType='slide'>
+            <View style={styles.inputContainer}>
+                <TextInput placeholder='Course Goal'
+                    style={styles.input} onChangeText={userInputHandler}
+                    value={userInput.input}
+                />
+                <View style={styles.buttonContainer}>
+                    <View style={styles.button}>
+                        <Button title='Add' onPress={props.onAddGoal.bind(this, userInput.input, setUserInput)} />
+                    </View>
+                    <View style={styles.button}>
+                        <Button title='Cancel' color='red' onPress={props.cancel} />
+                    </View>
+                </View>
+            </View>
+        </Modal>
     );
 };
 
 const styles = StyleSheet.create({
     inputContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
+        flex: 1,
+        justifyContent: 'center',
         alignItems: 'center'
     },
 
     input: {
-        width: 200,
+        width: 300,
         borderBottomColor: 'black',
         borderWidth: 1,
-        padding: 2
+        padding: 2,
+        marginBottom: 10
     },
+
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '60%'
+    },
+
+    button: {
+        width: '40%'
+    }
 });
 
 export default GoalInput;
