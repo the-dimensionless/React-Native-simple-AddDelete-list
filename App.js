85c39c5eae7464f78aebc6a78ea7724a45bed21d
the-dimensionless <sumit.sssingh.singh45@gmail.com>
2020-08-25 01:38:24 +0530

    Listing goals on pressing Add button

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, FlatList } from 'react-native';
 
 export default function App() {
+
+  const [userInput, setUserInput] = useState('');
+  const [goals, setGoals] = useState([]);
+
+  const userInputHandler = (input) => {
+    console.log('User entered ', input);
+    setUserInput({ input });
+  };
+
+  const addGoalHandler = () => {
+    console.log('User entered goal as ', userInput.input);
+    console.log('\nUpdating . . .\n');
+
+    setGoals(currentGoals => [...currentGoals,
+    {
+      key: Math.random().toString(),
+      value: userInput.input
+    }]);
+
+    // Clearing user inputhhh
+    setUserInput('');
+  };
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
         <TextInput placeholder='Course Goal'
-          style={styles.input} />
+          style={styles.input} onChangeText={userInputHandler}
+          value={userInput.input}
+        />
 
-        <Button title='Add' />
+        <Button title='Add' onPress={addGoalHandler} />
       </View>
 
-      <View>
-
-      </View>
+      <FlatList
+        data={goals}
+        renderItem={itemData => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
@@ -35,5 +65,13 @@ const styles = StyleSheet.create({
     borderBottomColor: 'black',
     borderWidth: 1,
     padding: 2
+  },
+
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: 'green',
+    borderColor: 'black',
+    borderWidth: 1
   }
 });
